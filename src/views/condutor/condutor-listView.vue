7312fb4140b8a4ae1367fd18d626f629fe54b77c
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-06-17 20:59:05 -0300

    Criando tabela e formulário das configurações

--- src/views/condutor/condutor-listView.vue
+++ src/views/condutor/condutor-listView.vue
@@ -142,6 +142,25 @@
             </tbody>
         </table>
     </div>
+
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalhes do condutor</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-start">
+                    Detalhes do condutor
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
+                    <!-- <button type="button" class="btn btn-primary">Understood</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
   
 <script lang="ts">
--- src/views/configuracao/criar-configuracaoView.vue
+++ src/views/configuracao/criar-configuracaoView.vue
@@ -1,5 +1,70 @@
 <template>
-
+    <section class="container col-12">
+        <h1 class="text-start my-4">Cadastrar configuração</h1>
+        <div class="row">
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Inicio do expediente</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Fim do expediente</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Valor Hora</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Valor Minuto</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Tempo de desconto</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Tempo para desconto</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Gerar desconto</label>
+                <div class="form-check form-switch m-0 text-center">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Vagas carro</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Vagas moto</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Vagas van</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+        </div>
+        <div class="d-flex flex-row justify-content-end">
+            <button type="button" class="btn btn-outline-secondary me-3">Cancelar</button>
+            <button type="button" class="btn btn-success">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-check-square" viewBox="0 0 16 16">
+                    <path
+                        d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    <path
+                        d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z" />
+                </svg>
+                Salvar
+            </button>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
