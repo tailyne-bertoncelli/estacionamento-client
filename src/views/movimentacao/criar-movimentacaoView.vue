3ff104203b970defe0965aa2ba3115a46c737555
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-06-17 22:27:47 -0300

    Criando tabela e formulario da movientação

--- src/views/movimentacao/criar-movimentacaoView.vue
+++ src/views/movimentacao/criar-movimentacaoView.vue
@@ -1,5 +1,47 @@
 <template>
-
+    <section class="container col-12">
+        <h1 class="text-start my-4">Cadastrar movimentação</h1>
+        <div class="row">
+            <div class="col-3 text-start">
+                <label for="exampleFormControlInput1" class="form-label">Entrada</label>
+                <input type="text" class="form-control mb-3" id="exampleFormControlInput1">
+            </div>
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Selecione o condutor:</label>
+                <select class="form-select" aria-label="Default select example">
+                    <!-- <option selected>Open this select menu</option> -->
+                    <option value="1">Maria Joaquina</option>
+                    <option value="2">Jacira Pietra</option>
+                    <option value="3">Whinderson Nunes</option>
+                </select>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col text-start">
+                <label for="exampleFormControlInput1" class="form-label">Selecione o veiculo:</label>
+                <select class="form-select" aria-label="Default select example">
+                    <!-- <option selected>Open this select menu</option> -->
+                    <option value="1">Velar Azul 2020</option>
+                    <option value="2">Mustang Vermelho 2022</option>
+                    <option value="3">321i 2023</option>
+                </select>
+            </div>
+        </div>
+        
+        <div class="d-flex flex-row justify-content-end">
+            <button type="button" class="btn btn-outline-secondary me-3">Cancelar</button>
+            <button type="button" class="btn btn-success">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-check-square" viewBox="0 0 16 16">
+                    <path
+                        d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    <path
+                        d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z" />
+                </svg>
+                Salvar
+            </button>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
